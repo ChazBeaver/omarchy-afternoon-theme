2b27924ffd022672d1ecd4b401333ccef0bbb0df
Chaz Beaver <HyprCore@Arch.com>
2026-03-27 19:31:59 -0400

    init theme

---
+++ neovim.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    dir = "ChazBeaver/afternoon.nvim",
+    name = "afternoon",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("afternoon")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "afternoon",
+    },
+  },
+}
